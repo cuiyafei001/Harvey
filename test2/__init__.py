1c919461acef966742d9f4f1f7dccd9117d78c11
cuiyafei <xxxx>
2017-12-13 15:42:55 +0800

    python基础语法练习

--- test2/__init__.py
+++ test2/__init__.py
@@ -1,10 +1,16 @@
 # _*_ coding: UTF-8 _*_
+from selenium import webdriver
+
+# driver = webdriver.Chrome()
+# url = "https://www.baidu.com/"
+# driver.get(url)
 
 
-x = 1
+# x = 1
+#
+# def fun1():
+#     print 'modulea',x
+#
+# def fun2():
+#     print 'modulea',x*2
 
-def fun1():
-    print 'modulea',x
-
-def fun2():
-    print 'modulea',x*2
