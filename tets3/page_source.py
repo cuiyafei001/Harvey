c349e8a4d42e3e883c63343131bdb94b53e3d18f
cuiyafei <xxxx>
2017-12-27 18:01:49 +0800

    page_source爬页面源码

--- tets3/page_source.py
+++ tets3/page_source.py
@@ -1,1 +1,9 @@
 # _*_ coding: UTF-8 _*_
+from selenium import webdriver
+
+driver = webdriver.Firefox()
+driver.get("http://www.cnblogs.com/cyfyywfc/")
+
+# 使用selenium提供的page_source方法获取页面源码
+page = driver.page_source
+print page
